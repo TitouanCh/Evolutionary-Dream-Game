566210b52df653491913a9e5f70127d842dd8262
TitouanCh <titouan.azert@wadsa.com>
2022-06-07 20:02:33 +0200

    cleanup

--- scripts/global.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-var paused = false
-
-func _process(delta):
-	if Input.is_action_just_pressed("pause"):
-		paused = !paused
--- scripts/mathUtilities.gd
+++
@@ -1,9 +0,0 @@
-extends Reference
-
-class_name AdvMath
-
-static func calc_poly(p, x):
-	var sum = 0
-	for i in range(len(p)):
-		sum += pow(x, i) * p[i]
-	return sum
